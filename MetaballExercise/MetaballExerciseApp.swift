1b7a03a7f7c6cd139d7cd800020d598b38cde677
LBkos <constantin.l.o.p@gmail.com>
2023-07-24 15:21:18 +0700

    Initial Commit

---
+++ MetaballExercise/MetaballExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaballExerciseApp.swift
+//  MetaballExercise
+//
+//  Created by Константин Лопаткин on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MetaballExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
